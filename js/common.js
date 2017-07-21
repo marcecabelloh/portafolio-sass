a3c5f597f1fd37158c3add355ca14537adffe4da
marcecabelloherrera <marcecabello.h@gmail.com>
2017-07-21 11:43:44 -0400

    estilos sass, navbar y carrusel

--- js/common.js
+++ js/common.js
@@ -1,5 +1,18 @@
 //plugins materialize
 $(document).ready(function(){
-	$('.carousel.carousel-slider').carousel({fullWidth: true});
+	$('.carousel.carousel-slider').carousel();
 })
+
+//plugin que permite q dropdown se deslice al momento de pasar el mouse por encima 
+ $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+  );
  
